97dc50f22fe138b7f6bf51680b3b58b93351130f
leoramosdiniz <leonardo_ramos_diniz@hotmail.com>
2023-06-19 16:29:19 -0300

    Add pág web

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import { collection, getDocs } from 'firebase/firestore/lite'
 
 function App() {
 
+  if(window.innerHeight <= 800){
+    maxHeight = window.innerHeight
+  }
+  let maxHeight;
+
   const [video, setVideos] = useState([])
 
   async function getVideos() {
@@ -24,7 +29,7 @@ function App() {
 
 
   return (
-    <div className="App">
+    <div className="App" style={{ maxHeight: maxHeight + "px" }}>
       <div className="app__videos">
         {video.map((item) => {
           return (
